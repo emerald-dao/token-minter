10707823a1c8f7b328660cd8a14e062d3e7c5be0
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-04-17 21:23:59 +0200

    added prismjs natively

---
+++ src/lib/components/PrismJS.svelte
@@ -0,0 +1,21 @@
+
+
+<script>
+  import Prism from 'prismjs';
+  import 'prismjs/themes/prism.css';
+  import "prismjs/components/prism-swift.js";
+  
+  export let code;
+  
+</script>
+
+<style>
+  pre {
+    padding: 1rem;
+    background-color: #f9f9f9;
+    border-radius:0.2rem;
+  }
+</style>
+
+<pre><code>{@html Prism.highlight(code, Prism.languages.swift, 'swift')}</code></pre>
+
