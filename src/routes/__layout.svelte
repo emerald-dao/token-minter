387c9cc1159ab5fa2f82e53f43c5b6528593a47a
mateoroldos <mateoroldos19@gmail.com>
2022-04-29 11:31:11 -0300

    fix: Apply CSS reset globally

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,11 @@
 <script>
-  import Header from "$lib/header/Header.svelte";
 </script>
-
-<!-- <Header /> -->
 
 <main class="container">
   <slot />
 </main>
 
-<style type="text/scss">	
+<style type="text/scss">
 	main {
     background-color: var(--cr-background-primary);
   }
