3da58a189492e32a5c3760ecd943180f0e4fd27c
mateoroldos <mateoroldos19@gmail.com>
2022-05-24 12:01:57 -0300

    fix: Stack component fix

--- src/lib/components/atoms/Stack.svelte
+++ src/lib/components/atoms/Stack.svelte
@@ -2,10 +2,10 @@
   export let direction = "column";
   export let gap = "2rem";
   export let align = "center";
-  export let justify = "space-between";
+  export let justify = "flex-start";
 </script>
 
-<div style="flex-direction: {direction}; gap: {gap}; align-items: {align}; justify-content={justify}">
+<div style="flex-direction: {direction}; gap: {gap}; align-items: {align}; justify-content: {justify};">
   <slot/>
 </div>
 
