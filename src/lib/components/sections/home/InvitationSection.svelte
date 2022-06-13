42b494f84d22ed2b11edb992341af403bab18ccc
mateoroldos <mateoroldos19@gmail.com>
2022-06-13 16:39:17 -0300

    feat: InvitationSection styling

--- src/lib/components/sections/home/InvitationSection.svelte
+++ src/lib/components/sections/home/InvitationSection.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { Section, Container, Button, Stack, Tagline } from "$lib/components/atoms/index";
+	import { Section, Container, Button, Stack, Tagline, TransparentCard } from "$lib/components/atoms/index";
 </script>
 
 <Section>
 	<Container>
-		<div class="invitation">
+		<TransparentCard accent={true} padding="4rem">
 			<Stack>
 				<Tagline>Emerald City DAO</Tagline>
 				<h2>Join the community</h2>
@@ -15,25 +15,18 @@
 				</p>
 				<Button>Join Discord</Button>
 			</Stack>
-		</div>
+		</TransparentCard>
 	</Container>
 </Section>
 
 <style type="scss">
-  .invitation {
-		background-color: var(--clr-accent-hover);
-    border-radius: 0.4rem;
-		padding: 4rem;
-		box-shadow: var(--clr-accent-main) 8px 8px, var(--clr-accent-soft) 16px 16px;
+	h2 {
+		color: var(--clr-font-heading);
+		text-align: center;
+	}
 
-		h2 {
-			color: var(--clr-font-heading);
-			text-align: center;
-		}
-
-		p {
-			text-align: center;
-			color: var(--clr-font-text);
-		}
+	p {
+		text-align: center;
+		color: var(--clr-font-text);
 	}
 </style>
