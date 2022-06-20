559db6242fde3ff0151f4b0a85cfe1be20a82dd1
mateoroldos <mateoroldos19@gmail.com>
2022-06-20 16:34:47 -0300

    fix: Fix minor bug in store interfaces

--- src/lib/generator/stores/CsvStore.ts
+++ src/lib/generator/stores/CsvStore.ts
@@ -1,5 +1,5 @@
 import type { Writable } from 'svelte/types/runtime/store';
-import type { FileState, ErrorMessages } from './interfaces';
+import type { FileState } from './interfaces';
 import { writable } from 'svelte/store';
 
 export const csvState: Writable<FileState> = writable({
