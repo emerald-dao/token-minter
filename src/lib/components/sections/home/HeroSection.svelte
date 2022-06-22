1a7beaa0100eb36cd8482ace14c3ba7a97473732
mateoroldos <mateoroldos19@gmail.com>
2022-06-22 10:57:33 -0300

    feat: Use Stack on HeroSection

--- src/lib/components/sections/home/HeroSection.svelte
+++ src/lib/components/sections/home/HeroSection.svelte
@@ -8,25 +8,25 @@
 		<AdaptableGrid>
 			<Stack align="start">
 				<h1>Tools for creating 
-					<span>NFT Collections and Contracts</span> 
+					<span class="accent">NFT Collections and Contracts</span> 
 					in the Flow Blockchain
 				</h1>
 				<p>Cupidatat amet irure adipisicing ut. Esse incididunt aliquip ex enim nostrud magna ea nostrud id mollit occaecat. Duis id voluptate veniam tempor velit id fugiat ut.</p>
 				<Button>Start</Button>
 			</Stack>
-			<div class="grid-child">
+			<Stack align="center" justify="center">
 				<video width="100%" height="100%" autoplay loop muted>
 					<source src="emerald.webm" type="video/webm">
 					<track kind="captions"/>
 					Your browser does not support the video tag.
 				</video>
-			<div/>
+			</Stack>
 		</AdaptableGrid>
 	</Container>
 </Section>
 <Marquee repeat="{10}">
 		<div class="marquee-element">
-			CRAFTED BY EMERALD CITY DAO
+			<span>CRAFTED WITH 💜 BY&nbsp;</span> EMERALD CITY DAO
 		</div>
 		<div class="marquee-element emerald-wrapper">
 			<Icon icon="ion:diamond-outline"/>
@@ -40,8 +40,7 @@
 		text-shadow: none;
 		
 		span {
-			color: var(--clr-primary-main);
-			text-shadow: 3px 3px 0 var(--clr-accent-soft);
+			color: var(--clr-accent-main);
 		}
 	}
 
@@ -49,11 +48,17 @@
     display: flex;
     align-items: center;
     justify-content: center;
-		margin-inline: 3rem
+		margin-inline: 3rem;
+		letter-spacing: 0.3em;
+		font-size: var(--fs-200);
+		
+		span {
+			font-style: italic;
+		}
   }
 
 	.emerald-wrapper {
 		margin-top: -0.3rem;
+		color: var(--clr-primary-main)
 	}
-
 </style>
