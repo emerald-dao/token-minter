650b22f3024ee19e9cd9077d0f5762a27f5e90c9
mateoroldos <mateoroldos19@gmail.com>
2022-06-27 12:11:29 -0300

    feat: Make generator step persistent

---
+++ src/lib/stores/generator/GeneratorGeneralStore.js
@@ -0,0 +1,15 @@
+import { writable } from 'svelte/store';
+import { browser } from '$app/env';
+
+export const step = writable((browser && localStorage.getItem('step')) || 0);
+
+step.subscribe((val) => {
+  if (browser) return (localStorage.step = val);
+});
+
+export const onNext = () => {
+  step.update((current) => Number(current) + 1);
+};
+export const onBack = () => {
+  step.update((current) => Number(current) - 1);
+};
