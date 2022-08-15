f84c8eed2817434b30a7a93cd035e4d4933bdd20
Mateo Roldos <mateoroldos19@gmail.com>
2022-08-15 10:14:48 -0300

    fix: Typo fix

--- src/lib/components/sections/home/InvitationSection.svelte
+++ src/lib/components/sections/home/InvitationSection.svelte
@@ -11,7 +11,7 @@
 				<p>
 					We are the first DAO on the Flow Blockchain 
 					<br/>
-					<b>Educate, build and goverm</b>
+					<b>Educate, build and govern</b>
 				</p>
 				<Button leftIcon="logo-discord" target="_blank" href="https://discord.com/invite/emeraldcity">Join Discord</Button>
 			</Stack>
