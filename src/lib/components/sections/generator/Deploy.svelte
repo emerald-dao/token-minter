45e91ea2cce37d57b74160eef97311a415a4de27
Mateo Roldos <mateoroldos19@gmail.com>
2022-08-15 10:04:40 -0300

    fix: More styling fixes to generator

--- src/lib/components/sections/generator/Deploy.svelte
+++ src/lib/components/sections/generator/Deploy.svelte
@@ -2,22 +2,25 @@
   import { Button, CollectionPreview, Stack } from "$lib/components/atoms/index";
   import DeploymentTransaction from '$lib/components/sections/generator/DeploymentTransaction.svelte';
   import { deployToMainnet, deployToTestnet } from "../../../../flow/actions.js";
-	import { transactionInProgress, contractCode, contractInfo } from "../../../../flow/stores";
+	import { transactionInProgress } from "../../../../flow/stores";
   import GeneratorStepLayout from "./GeneratorStepLayout.svelte";
 </script>
 
-{#if $transactionInProgress}
-  <DeploymentTransaction/>
-{:else}
-  <GeneratorStepLayout>
-    <CollectionPreview slot="main-content"/>
-    <Stack slot="buttons" direction="row ">
-      <Button on:click={deployToTestnet} leftIcon="flask" class="ghost">
-        Deploy to Testnet
-      </Button>
-      <Button on:click={deployToMainnet} leftIcon="rocket">
-        Deploy to Mainnet
-      </Button>
+
+<GeneratorStepLayout>
+  <div slot="main-content">    
+    {#if $transactionInProgress}
+      <DeploymentTransaction/>
+    {:else}
+      <CollectionPreview/>
+    {/if}
+  </div>
+  <Stack slot="buttons" direction="row ">
+    <Button on:click={deployToTestnet} leftIcon="flask" class="ghost">
+      Deploy to Testnet
+    </Button>
+    <Button on:click={deployToMainnet} leftIcon="rocket">
+      Deploy to Mainnet
+    </Button>
     </Stack>
-  </GeneratorStepLayout>
-{/if}
+</GeneratorStepLayout>
--- src/lib/components/sections/generator/DeploymentTransaction.svelte
+++ src/lib/components/sections/generator/DeploymentTransaction.svelte
@@ -1,22 +1,9 @@
 <script>
-  import { Container, Section} from "$lib/components/atoms/index";
+  import { TransparentCard } from "$lib/components/atoms/index";
   import Transaction from "$lib/components/flow/Transaction.svelte";
 </script>
 
-<Section>
-  <Container>
-    <div>
-      <Transaction/>
-    </div>
-  </Container>
-</Section>
 
-<style type="scss">
-  div {
-    color: var(--clr-font-text-inverse);
-    background-color: var(--clr-primary-main);
-    border-radius: 0.4rem;
-		padding: 4rem;
-		box-shadow: var(--clr-accent-soft) 8px 8px, var(--clr-accent-soft-t6) 16px 16px;
-  }
-</style>
+<TransparentCard>
+  <Transaction/>
+</TransparentCard>
