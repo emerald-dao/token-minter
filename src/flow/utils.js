459dbc8a5545cec8cc62dcb35b2e76701055e50b
Jacob <jacobtucker818@gmail.com>
2022-08-22 11:29:18 -0400

    find profile

--- src/flow/utils.js
+++ src/flow/utils.js
@@ -102,14 +102,30 @@ export const resolveAddressObject = async (lookup) => {
   }
 }
 
-export const getFindName = async (address) => {
+export const getFindProfile = async (address) => {
   try {
       return await fcl.query({
         cadence: `
         import FIND from ${get(addresses).FIND}
-        pub fun main(address: Address): String? {
-            let name = FIND.reverseLookup(address)
-            return name?.concat(".find")
+        pub fun main(address: Address): Profile? {
+            if let name = FIND.reverseLookup(address) {
+              let profile = FIND.lookup(name)!
+              return Profile(_name: name, _address: address, _avatar: profile.getAvatar())
+            }
+            
+            return nil
+        }
+
+        pub struct Profile {
+          pub let name: String
+          pub let address: Address
+          pub let avatar: String
+
+          init(_name: String, _address: Address, _avatar: String) {
+            self.name = _name
+            self.address = _address
+            self.avatar = _avatar
+          }
         }
         `,
         args: (arg, t) => [
