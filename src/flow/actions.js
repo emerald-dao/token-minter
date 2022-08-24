d7734dc0946ab510967f1ecf9ef5f9cca954e6ba
jacob-tucker <jacobtucker818@gmail.com>
2022-08-24 15:44:06 -0400

    blocto fixed it yay

--- src/flow/actions.js
+++ src/flow/actions.js
@@ -61,8 +61,7 @@ function switchNetwork(newNetwork) {
     fcl
       .config()
       .put('accessNode.api', 'https://rest-testnet.onflow.org')
-      .put('discovery.wallet', "https://flow-wallet-testnet.blocto.app/api/flow/authn")
-      .put("discovery.wallet.method", "HTTP/POST")
+      .put('discovery.wallet', "https://fcl-discovery.onflow.org/testnet/authn")
   } else if (newNetwork === 'mainnet') {
     fcl
       .config()
