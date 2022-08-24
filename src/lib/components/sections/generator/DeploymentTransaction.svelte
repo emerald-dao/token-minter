d890de9571de5f369aff437d9866ebd1bddae5c3
mateoroldos <mateoroldos19@gmail.com>
2022-08-24 18:06:45 -0300

    fix: Roll back to previous DeploymentTransaction component

--- src/lib/components/sections/generator/DeploymentTransaction.svelte
+++ src/lib/components/sections/generator/DeploymentTransaction.svelte
@@ -1,9 +1,22 @@
 <script>
-  import { TransparentCard } from "$lib/components/atoms/index";
+  import { Container, Section} from "$lib/components/atoms/index";
   import Transaction from "$lib/components/flow/Transaction.svelte";
 </script>
 
+<Section>
+  <Container>
+    <div>
+      <Transaction/>
+    </div>
+  </Container>
+</Section>
 
-<TransparentCard>
-  <Transaction/>
-</TransparentCard>
+<style type="scss">
+  div {
+    color: var(--clr-font-text-inverse);
+    background-color: var(--clr-primary-main);
+    border-radius: 0.4rem;
+		padding: 4rem;
+		box-shadow: var(--clr-accent-soft) 8px 8px, var(--clr-accent-soft-t6) 16px 16px;
+  }
+</style>
