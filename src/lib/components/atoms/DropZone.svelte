a8fd04e71e8b5c09ea4118dc384b3c042eccfe35
mateoroldos <mateoroldos19@gmail.com>
2022-08-30 16:27:41 -0300

    feat: Make DropZone more extensible and reusable

--- src/lib/components/atoms/DropZone.svelte
+++ src/lib/components/atoms/DropZone.svelte
@@ -1,74 +1,81 @@
 <script>
+  import DropZoneFile from "./DropZoneFile.svelte";
   import Icon from "@iconify/svelte";
-  import { DropZoneFile } from "$lib/components/atoms/index";
-  import { emptyCsvStore } from "$lib/stores/generator/CsvStore.ts";
-  import { setImagesStateToIdle } from "$lib/stores/generator/ImagesStore";
+  let dragOver = false; // Flag to check if we are dragging over the dropzone
+  let inputRef; // Reference to the hidden input element
+  
+  export let name;
+  export let id;
+  export let fileStore;
+  export let deleteFileFromStore;
+  export let deleteAllFilesFromStore;
+  export let errors; 
+  export let placeholder = "Drop file here or click to upload";
+  export let type; // Type of file we are expecting
+  
+  export let saveFunction = (files) => {
+    fileStore.update((store) => {
+      store = [...store, ...files]
+    })
+  };
 
   const handleFileDrop = async (e) => {
-    console.log(e.dataTransfer);
     e.preventDefault();
     dragOver = false;
-    dropHandlingFunction(e.dataTransfer);
+    saveFunction(e.dataTransfer);
   };
 
   const onInput = (e) => {
-    console.log(e.target.files);
-    console.log(e.target);
     e.preventDefault();
     dragOver = false;
-    dropHandlingFunction(e.target);
+    saveFunction(e.target);
   };
-
-  let dragOver = false; // Flag to check if we are dragging over the dropzone
-
-  export let fileStore;
-  export let fileState;
-  export let dropHandlingFunction; // Function containing validation logic + parsing logic + storing logic
-  export let promptText = "Drop file here or click to upload";
-  export let type; // Type of file we are expecting
-
-  let inputRef; // Reference to the hidden input element
 </script>
 
 <div
   class="drop-zone"
   class:drop-zone-over={dragOver}
-  class:drop-zone-error={fileState.errorMessages.length > 0}
+  class:drop-zone-error={errors && errors.length > 0}
   on:dragover={() => (dragOver = true)}
   on:dragleave={() => (dragOver = false)}
   on:dragend={() => (dragOver = false)}
   on:drop={handleFileDrop}
   on:click={() => inputRef.click()}
   ondragover="return false">
-  {#if fileStore && fileStore.length > 0 && type === "image"}
+
+  {#if fileStore && fileStore.length > 2}
+    <div class="empty-all-wrapper">
+      <div on:click|stopPropagation={deleteAllFilesFromStore()} class="empty-all-icon-wrapper">
+        <Icon icon="ion:trash" color="var(--clr-accent-main)"/>
+      </div>
+    </div>
+  {/if}
+
+  {#if fileStore && fileStore.length > 0}
     {#each fileStore as file, index}
       <DropZoneFile
-        {file}
-        on:deleteFile={() => {
-          fileStore.splice(index, 1);
-          fileStore = fileStore;
-          if (fileStore.length === 0) {
-            setImagesStateToIdle();
-          }
-        }} />
+      {file}
+      on:deleteFile={deleteFileFromStore(index)} />
     {/each}
-  {:else if !(fileStore === null) && type === "csv"}
-    <DropZoneFile file={fileStore} on:deleteFile={() => emptyCsvStore()} />
   {:else}
-    <Icon icon="ion:cloud-upload-outline" />
-    <span class="prompt">{promptText}</span>
-    {#if fileState.errorMessages.length > 0}
-      {#each fileState.errorMessages as error}
-        <p class="error">{error}</p>
-      {/each}
-    {/if}
+    <Icon icon="ion:cloud-upload-outline" color="var(--clr-accent-main-t3)" />
+    <span class="prompt">{placeholder}</span>
   {/if}
+  {#if errors && errors.length > 0}
+    {#each errors as error}
+      <p class="error">{error}</p>
+    {/each}
+  {/if}
+
   <!-- Add a hidden input element to trigger when the user clicks the drop zone -->
-  {#if type === "image"}
-    <input type="file" bind:this={inputRef} on:input={onInput} webkitdirectory directory multiple>
+  {#if type === "images-folder"}
+    <input name={name} id={id} type="file" bind:this={inputRef} on:input={onInput} webkitdirectory directory multiple>
+  {:else if type === "image"}
+    <input name={name} id={id} type="file" bind:this={inputRef} on:input={onInput}>
   {:else}
-    <input type="file" bind:this={inputRef} on:input={onInput}>
+    <input name={name} id={id} type="file" bind:this={inputRef} on:input={onInput}>
   {/if}
+
 </div>
 
 <style type="scss">
@@ -85,7 +92,7 @@
     text-align: center;
     cursor: pointer;
     color: var(--clr-font-text);
-     border: 2px var(--clr-accent-main-t5) solid;
+    border: 2px var(--clr-accent-main-t5) solid;
     background-color: var(--clr-accent-main-t9);
     border-radius: 10px;
     transition: 0.5s;
@@ -94,12 +101,34 @@
 
     .prompt {
       font-size: var(--fs-300);
+      color: var(--clr-accent-main-t3);
+    }
+
+    .empty-all-wrapper {
+      width: 100%;
+      position: sticky;
+      top: -1rem;
+      margin-top: -3.8rem;
+      margin-right: -2rem;
+      z-index: 100;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+
+      .empty-all-icon-wrapper {
+        background-color: var(--clr-accent-main-t8);
+        padding: 0.4rem;
+        border-radius: 0.4rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 2px var(--clr-accent-main-t7) solid;
+      }
     }
   }
 
   .drop-zone-over {
-    color: var(--clr-font-text-inverse);
-    background-color: var(--clr-primary-main);
+    background-color: var(--clr-accent-main-t6);
   }
   .drop-zone-error {
     border-color: red;
