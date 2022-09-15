6e380e9d6544b50626fa2c18e3a572f1c56ff4b5
mateoroldos <mateoroldos19@gmail.com>
2022-09-15 17:39:27 -0300

    fix: Fix DropZone display thumnail error

--- src/lib/components/atoms/DropZoneFile.svelte
+++ src/lib/components/atoms/DropZoneFile.svelte
@@ -1,25 +1,25 @@
 <script>
   import Icon from "@iconify/svelte";
-  import { afterUpdate } from 'svelte';
+  import { onMount } from 'svelte';
   import { createEventDispatcher } from 'svelte';
 
-  afterUpdate (() => {
-		displayThumbnail(file);
+  onMount (() => {
+    if(file?.type && file?.type.startsWith('image/')) {
+      displayThumbnail(file);
+    }
 	});
 
   const displayThumbnail = (file) => {
-    if (file.type && file.type.startsWith('image/')) { 
-      const reader = new FileReader();
-      reader.readAsDataURL(file); // base 64 format
-      
-      reader.onload = () => {
-        thumbnailElement.style.backgroundImage = `url('${reader.result}')`; /*asynchronous call. This function runs once reader is done reading file. reader.result is the base 64 format*/
-        thumbnailElement.style.height = "100%";
-        thumbnailElement.style.aspectRatio = "1/1";
-        thumbnailElement.style.backgroundSize = "cover";
-        thumbnailElement.style.display = "flex";
-        thumbnailElement.style.borderRadius = "0.2rem";
-      };
+    const reader = new FileReader();
+    reader.readAsDataURL(file); // base 64 format
+    
+    reader.onload = () => {
+      thumbnailElement.style.backgroundImage = `url('${reader.result}')`; /*asynchronous call. This function runs once reader is done reading file. reader.result is the base 64 format*/
+      thumbnailElement.style.height = "100%";
+      thumbnailElement.style.aspectRatio = "1/1";
+      thumbnailElement.style.backgroundSize = "cover";
+      thumbnailElement.style.display = "flex";
+      thumbnailElement.style.borderRadius = "0.2rem";
     };
   };
 
