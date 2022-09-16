d32ad557f590bae2024f661520d0bd6f68a0e7a2
mateoroldos <mateoroldos19@gmail.com>
2022-09-16 12:13:00 -0300

    feat: Improve DropZoneFile image preview

--- src/lib/components/atoms/DropZoneFile.svelte
+++ src/lib/components/atoms/DropZoneFile.svelte
@@ -16,8 +16,11 @@
     reader.onload = () => {
       thumbnailElement.style.backgroundImage = `url('${reader.result}')`; /*asynchronous call. This function runs once reader is done reading file. reader.result is the base 64 format*/
       thumbnailElement.style.height = "100%";
+      thumbnailElement.style.width = "60px";
       thumbnailElement.style.aspectRatio = "1/1";
-      thumbnailElement.style.backgroundSize = "cover";
+      thumbnailElement.style.backgroundSize = "contain";
+      thumbnailElement.style.backgroundRepeat = "no-repeat";
+      thumbnailElement.style.backgroundPosition = "center";
       thumbnailElement.style.display = "flex";
       thumbnailElement.style.borderRadius = "0.2rem";
     };
@@ -30,10 +33,10 @@
 </script>
 
 <div class="drop-zone-file">
-  {#if file.type && file.type.startsWith('image/')}
-    <div bind:this={thumbnailElement} class="thumbnailElement"/>
-  {/if}
   <div class="file-data">
+    {#if file.type && file.type.startsWith('image/')}
+      <div bind:this={thumbnailElement} class="thumbnailElement"/>
+    {/if}
     <h5>{file.name}</h5>
   </div>
   <div on:click|stopPropagation={() => dispatch('deleteFile')}>
@@ -65,16 +68,17 @@
     h5 {
       color: var(--clr-accent-main)
     }
-
-    .thumbnailElement {
-      display: none;
-    }
-
+    
     .file-data {
       display: flex;
       flex-direction: row;
-      align-items: center;
+      align-items: flex-start;
       gap: 2rem;
+      width: 100%;
+
+      .thumbnailElement {
+        display: none;
+      }
     }
   }
 </style>
