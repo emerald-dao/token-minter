84457e41803262002cdb193e3931803cc47c8ca2
jacob-tucker <jacobtucker818@gmail.com>
2022-09-18 22:56:00 -0400

    featured

--- src/lib/components/sections/home/FeaturedCollections.svelte
+++ src/lib/components/sections/home/FeaturedCollections.svelte
@@ -1,63 +1,56 @@
 <script>
-	import {
-		Section,
-		Container,
-		CollectionCard,
-		Stack,
-	} from "$atoms";
+  import { Section, Container, CollectionCard, Stack } from "$atoms";
 
   let collections = [
     {
-      name: "Hats NFT 6",
-      slug: "HatsNFT6",
-      thumbnailURL: "/images/guide/ballerz.png",
+      name: "Official Testnet Test",
+      slug: "OfficialTestnetTest",
+      thumbnailURL: "/nft_cap1.png",
       description:
-        "Hats NFT 6 created their collection on Touchstone. Founded in 2016, they have been OGs in the crypto space...!",
-      owner: "0x6c0d53c676256e8c",
+        "This is our first official test of Touchstone on Testnet. NOTE: Because this is on testnet, this is not a real collection.",
+      owner: "0xfe10201e647ca718",
     },
-    {
-      name: "Ballerz",
-      slug: "ballerz",
-      thumbnailURL: "/images/guide/ballerz.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      owner: "0x0da118e8ae345d95",
-    }
   ];
 </script>
 
 <div class="background">
-	<Section>
-		<Container width="large">
-			<Stack>
-				<h2>Trending Collections</h2>
-				<Stack direction="column">
-					{#each collections as collection}
+  <Section>
+    <Container width="large">
+      <Stack>
+        <h2>Trending Collections</h2>
+        <Stack direction="column">
+          {#each collections as collection}
             <CollectionCard
               name={collection.name}
               url={`/discover/${collection.owner}/${collection.slug}`}
               thumbnailURL={collection.thumbnailURL}
               description={collection.description}
-              owner={collection.owner} 
-            />
-					{/each}
-				</Stack>
-			</Stack>
-		</Container>
-	</Section>
+              owner={collection.owner} />
+          {/each}
+        </Stack>
+      </Stack>
+    </Container>
+  </Section>
 </div>
 
 <style type="scss">
-	h2 {
-		padding-bottom: 2rem;
-	}
-	.background {
-		border-top: 4px var(--clr-accent-main-t8) solid;
-    background: 
-      radial-gradient(ellipse at right, var(--clr-primary-main-t9), transparent),
+  h2 {
+    padding-bottom: 2rem;
+  }
+  .background {
+    border-top: 4px var(--clr-accent-main-t8) solid;
+    background: radial-gradient(
+        ellipse at right,
+        var(--clr-primary-main-t9),
+        transparent
+      ),
       radial-gradient(ellipse at left, var(--clr-primary-main-t9), transparent),
-      radial-gradient(ellipse at bottom, var(--clr-primary-soft-t4), transparent),
+      radial-gradient(
+        ellipse at bottom,
+        var(--clr-primary-soft-t4),
+        transparent
+      ),
       radial-gradient(ellipse at top, var(--clr-accent-soft-t4), transparent);
-		backdrop-filter: blur(50px);
-	}
+    backdrop-filter: blur(50px);
+  }
 </style>
