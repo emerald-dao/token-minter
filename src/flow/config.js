117c69ce0abb208861291fb9fa924bc2cd812910
jacob-tucker <jacobtucker818@gmail.com>
2022-09-29 00:52:04 -0500

    push last changes

--- src/flow/config.js
+++ src/flow/config.js
@@ -6,5 +6,7 @@ config()
   .put('app.detail.icon', 'https://i.imgur.com/KSL4gq0.png')
   .put('accessNode.api', 'https://rest-testnet.onflow.org')
   .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn');
+  // .put('accessNode.api', 'https://rest-mainnet.onflow.org')
+  // .put('discovery.wallet', 'https://fcl-discovery.onflow.org/authn');
 // .put('accessNode.api', 'http://localhost:8888')
 // .put('discovery.wallet', 'http://localhost:8701/fcl/authn')
