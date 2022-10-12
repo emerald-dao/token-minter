59cd57f0b0199b2e6229a806bf567ad01aa36009
Jacob <jacobtucker818@gmail.com>
2022-10-12 13:26:38 -0400

    feature geniace planets

--- src/lib/components/sections/home/FeaturedCollections.svelte
+++ src/lib/components/sections/home/FeaturedCollections.svelte
@@ -4,13 +4,20 @@
   let collections = [
     {
       name: "Emeralds",
-      slug: "Emeralds",
       thumbnailURL: "/emerald_2_thmb.png",
       description:
         "This is the first official Emerald City NFT collection, and the first ever collection on Touchstone. Featured in this collection are different types of Emeralds, all with different origins, planets, energetic properties, and elements. Purchasing these Emeralds will help pay developers inside of Emerald City to build more free tools like Touchstone, educate more users inside of Emerald Academy, and will grant you free months of Emerald Pass subscription.",
       owner: "Emerald City",
       url: "/Emeralds",
     },
+    {
+      name: "Maneki Planets",
+      thumbnailURL: "/geniace-planets.png",
+      description:
+        "Our iconic MANEKI planets, with just 224 lucky cats in this collection. With an inviting raised paw and pointy ears, these iconic lucky charms have been bringing good fortune for centuries. The MANEKI is the adopted icon of the GENIACE community and brings good fortune & benefits to those who hold a special MANEKI NFT",
+      owner: "Geniace",
+      url: "/discover/0xcf3c77ef638573e8/TouchstoneManekiPlanets",
+    }
   ];
 </script>
 
